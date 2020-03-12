7c2b42faf10d3e30ad728776c9ef2309b5cbf205
Sundus Malih <malihsundus@gmail.com>
2020-03-12 19:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add solution here
 
 
+<<<<<<< HEAD
 
 function theBeatlesPlay(musician,instrument) {
   var n = []
@@ -14,6 +15,21 @@ function johnLennonFacts(facts){
   var emptytwo = []
   let i = 0
   while (i < facts.length) {
+=======
+var n = []
+var musician = ["John Lennon", "Paul McCartney", "George Harrison"]
+var instrument = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+function theBeatlesPlay(array) {
+  for (let i = 0; i <4; i+=1) {
+   n.push('${musician[i]} plays ${instrument[i]}');
+return n}}
+
+var emptytwo = 0
+function johnLennonFacts(array){
+  let i = 0
+  while (i < 4) {
+>>>>>>> c25fe3295083ede7d02d0a54d021008402982e74
     emptytwo.push(`${facts[i]}!!!`)
     i +=1}
   return emptytwo
@@ -23,6 +39,7 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(n){
   var emptythree = []
 do{
+<<<<<<< HEAD
   emptythree.push(`I love the Beatles!`);
 n++
 } while(n < 15)
@@ -34,6 +51,15 @@ return emptythree
 
 
 
+=======
+  emptythree.push(`"I love the Beatles!"`);
+n++
+}
+while(n < 15){
+  return n
+}
+}
+>>>>>>> c25fe3295083ede7d02d0a54d021008402982e74
 
 //const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 //const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
